c893287211efa493b1a44ae4334e8e72c506a173
Saran <saranmvdm@gmail.com>
2024-01-21 16:07:44 +0530

    logic of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,81 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'blood-cell-compatibility';
+  bloodGroups: string[] = ['O-', 'O+', 'B-', 'B+', 'A-', 'A+', 'AB-', 'AB+'];
+  patientBloodGroup: string = '';
+  donorBloodGroup: string = '';
+  isCompatible: boolean | null = null;
+  isCheckDone: boolean = false;
+
+  constructor() {}
+  checkCompatibilty() {
+    if (this.patientBloodGroup === 'O-' && this.donorBloodGroup === 'O-') {
+      this.isCompatible = true;
+    } else if (
+      this.patientBloodGroup === 'O+' &&
+      (this.donorBloodGroup === 'O-' || this.donorBloodGroup === 'O+')
+    ) {
+      this.isCompatible = true;
+    } else if (
+      this.patientBloodGroup === 'B-' &&
+      (this.donorBloodGroup === 'B-' || this.donorBloodGroup === 'O-')
+    ) {
+      this.isCompatible = true;
+    } else if (
+      this.patientBloodGroup === 'B+' &&
+      (this.donorBloodGroup === 'B+' ||
+        this.donorBloodGroup === 'B-' ||
+        this.donorBloodGroup === 'O+' ||
+        this.donorBloodGroup === 'O-')
+    ) {
+      this.isCompatible = true;
+    } else if (
+      this.patientBloodGroup === 'A-' &&
+      (this.donorBloodGroup === 'A-' || this.donorBloodGroup === 'O-')
+    ) {
+      this.isCompatible = true;
+    } else if (
+      this.patientBloodGroup === 'A+' &&
+      (this.donorBloodGroup === 'A+' ||
+        this.donorBloodGroup === 'A-' ||
+        this.donorBloodGroup === 'O+' ||
+        this.donorBloodGroup === 'O-')
+    ) {
+      this.isCompatible = true;
+    } else if (
+      this.patientBloodGroup === 'AB-' &&
+      (this.donorBloodGroup === 'AB-' ||
+        this.donorBloodGroup === 'A-' ||
+        this.donorBloodGroup === 'B-' ||
+        this.donorBloodGroup === 'O-')
+    ) {
+      this.isCompatible = true;
+    } else if (
+      this.patientBloodGroup === 'AB+' &&
+      (this.donorBloodGroup === 'AB+' ||
+        this.donorBloodGroup === 'AB-' ||
+        this.donorBloodGroup === 'A+' ||
+        this.donorBloodGroup === 'A-' ||
+        this.donorBloodGroup === 'B+' ||
+        this.donorBloodGroup === 'B-' ||
+        this.donorBloodGroup === 'O+' ||
+        this.donorBloodGroup === 'O-')
+    ) {
+      this.isCompatible = true;
+    } else {
+      console.log('Not Compatible 👎');
+      this.isCheckDone = true;
+      this.isCompatible = false;
+    }
+  }
 }
